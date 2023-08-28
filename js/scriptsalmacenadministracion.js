01d2ed7cd7f558d03b1b06c9feb25994b341131e
Addany <appdata.123@gmail.com>
2023-08-28 12:57:55 -0600

    Cambios alpha

--- js/scriptsalmacenadministracion.js
+++ js/scriptsalmacenadministracion.js
@@ -1,190 +1,297 @@
-// Datos de ejemplo para el historial del almacén
-const historialAlmacen = [
+historialAlmacen = [
   {
-    id: 1,
-    nombre: 'Martillo',
-    color: 'Rojo',
-    tipo: 'Herramienta Manual',
-    fechaRegistro: '2023-07-20',
-    descripcion: 'Martillo de acero con mango de madera',
-    codigoQR: 'ABC123',
-    imagen: 'martillo.jpg'
+    id: 12345,
+    nombre: "Destornillador",
+    marca: "MarcaX",
+    ordenCompra: "12345",
+    tamaño: "5mm",
+    noSerie: "ABC123",
+    estado: "Nuevo",
+    color: "Rojo",
+    tipo: "Herramienta Manual",
+    fechaRegistro: "2023-08-01",
+    descripcion: "Destornillador plano",
+ 
   },
   {
     id: 2,
-    nombre: 'Taladro Inalámbrico',
-    color: 'Negro y Amarillo',
-    tipo: 'Eléctrica',
-    fechaRegistro: '2023-07-22',
-    descripcion: 'Taladro inalámbrico de 18V con accesorios',
-    codigoQR: 'XYZ789',
-    imagen: 'taladro.jpg'
-  }
-  // Agrega más datos aquí
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+  {
+    id: 2,
+    nombre: "Taladro",
+    marca: "MarcaY",
+    ordenCompra: "67890",
+    tamaño: "10mm",
+    noSerie: "DEF456",
+    estado: "Usado",
+    color: "Azul",
+    tipo: "Eléctrica",
+    fechaRegistro: "2023-08-15",
+    descripcion: "Taladro con velocidad variable",
+
+  },
+
+  // ... más datos
 ];
 
-// Función para abrir el popup de edición de herramienta
-function abrirPopupEdicion(herramienta) {
-  const popup = document.getElementById('popup');
-  const formEditarHerramienta = document.getElementById('editarHerramientaForm');
-  const inputId = document.getElementById('editId');
-  const inputNombre = document.getElementById('editNombre');
-  const inputColor = document.getElementById('editColor');
-  const inputTipo = document.getElementById('editTipo');
-  const inputFecha = document.getElementById('editFecha');
-  const inputDescripcion = document.getElementById('editDescripcion');
+document.addEventListener('DOMContentLoaded', (event) => {
+  generarTablaHistorial(historialAlmacen);
 
-  inputId.value = herramienta.id;
-  inputNombre.value = herramienta.nombre;
-  inputColor.value = herramienta.color;
-  inputTipo.value = herramienta.tipo;
-  inputFecha.value = herramienta.fechaRegistro;
-  inputDescripcion.value = herramienta.descripcion;
+  const buscador = document.getElementById('buscador');
+  buscador.oninput = () => buscarHerramienta(buscador.value);
 
-  formEditarHerramienta.onsubmit = (event) => {
+  const categoriaSelect = document.getElementById('categoria');
+  categoriaSelect.onchange = () => buscarPorCategoria(categoriaSelect.value);
+
+  const fechaInput = document.getElementById('fecha');
+  fechaInput.onchange = () => buscarPorFecha(fechaInput.value);
+
+  // Inicializar el popup
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+
+  document.getElementById("cancelarEdicion").addEventListener("click", function(event){
     event.preventDefault();
-    // Aquí puedes agregar la lógica para guardar los cambios de edición
-    // Puedes acceder a los datos ingresados por el usuario usando los valores de los campos inputId, inputNombre, etc.
-    alert('Herramienta editada correctamente');
-    popup.style.display = 'none';
-  };
-
-  const cancelarEdicionBtn = document.getElementById('cancelarEdicion');
-  cancelarEdicionBtn.onclick = () => {
-    popup.style.display = 'none';
-  };
-
-  popup.style.display = 'flex';
-}
-
-// Función para buscar herramientas por texto o ID
-function buscarHerramienta(texto) {
-  const textoBuscado = texto.trim().toLowerCase();
-  const historialFiltrado = historialAlmacen.filter(item => {
-    return (
-      item.id.toString().includes(textoBuscado) ||
-      item.nombre.toLowerCase().includes(textoBuscado) ||
-      item.color.toLowerCase().includes(textoBuscado) ||
-      item.tipo.toLowerCase().includes(textoBuscado) ||
-      item.descripcion.toLowerCase().includes(textoBuscado)
-    );
+    popup.style.display = "none";
+    cerrarPopup();
   });
-  generarFichasHistorial(historialFiltrado);
-}
 
-// Función para buscar herramientas por categoría
-function buscarPorCategoria(categoria) {
-  const historialFiltrado = historialAlmacen.filter(item => {
-    return categoria === 'todos' || item.tipo === categoria;
-  });
-  generarFichasHistorial(historialFiltrado);
-}
-
-// Función para buscar herramientas por fecha
-function buscarPorFecha(fecha) {
-  const historialFiltrado = historialAlmacen.filter(item => item.fechaRegistro === fecha);
-  generarFichasHistorial(historialFiltrado);
-}
-
-// Función para generar las fichas del historial del almacén
-function generarFichasHistorial(historial) {
-  const historyCards = document.querySelector('.history-cards');
-  historyCards.innerHTML = '';
-
-  historial.forEach(item => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-
-    const img = document.createElement('img');
-    img.src = `Resources/${item.imagen}`;
-    img.alt = item.nombre;
-    card.appendChild(img);
-
-    const id = document.createElement('p');
-    id.textContent = `ID: ${item.id}`;
-    card.appendChild(id);
-
-    const nombre = document.createElement('p');
-    nombre.textContent = `Nombre: ${item.nombre}`;
-    card.appendChild(nombre);
-
-    const color = document.createElement('p');
-    color.textContent = `Color: ${item.color}`;
-    card.appendChild(color);
-
-    const tipo = document.createElement('p');
-    tipo.textContent = `Tipo: ${item.tipo}`;
-    card.appendChild(tipo);
-
-    const fechaRegistro = document.createElement('p');
-    fechaRegistro.textContent = `Fecha de Registro: ${item.fechaRegistro}`;
-    card.appendChild(fechaRegistro);
-
-    const descripcion = document.createElement('p');
-    descripcion.textContent = `Descripción: ${item.descripcion}`;
-    card.appendChild(descripcion);
-
-    const action = document.createElement('div');
-    action.classList.add('action');
-
-    const qrButton = document.createElement('button');
-    qrButton.textContent = 'Descargar Código QR';
-    qrButton.onclick = () => {
-      // Aquí puedes agregar la lógica para descargar el código QR de la herramienta
-      alert(`Descargando Código QR de ${item.nombre}`);
-    };
-    action.appendChild(qrButton);
-
-    const editButton = document.createElement('button');
-    editButton.textContent = 'Editar';
-    editButton.onclick = () => {
-      abrirPopupEdicion(item);
-    };
-    action.appendChild(editButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Eliminar';
-    deleteButton.onclick = () => {
-      // Aquí puedes agregar la lógica para eliminar la herramienta
-      alert(`Eliminando ${item.nombre}`);
-    };
-    action.appendChild(deleteButton);
-
-    card.appendChild(action);
-
-    historyCards.appendChild(card);
-  });
-}
-
-// Llamamos a la función para generar las fichas del historial del almacén cuando la página se carga
-window.addEventListener('DOMContentLoaded', () => {
-  generarFichasHistorial(historialAlmacen);
-
-  // Obtener elementos del formulario de búsqueda
-  const formBuscar = document.querySelector('.form-buscar');
-  const inputTexto = formBuscar.querySelector('#textoBuscar');
-  const inputFecha = formBuscar.querySelector('#fechaBuscar');
-  const selectCategoria = formBuscar.querySelector('#categoria');
-
-  // Agregar evento de búsqueda por texto o ID
-  formBuscar.addEventListener('submit', (event) => {
+  document.getElementById("editarHerramientaForm").addEventListener("submit", function(event){
     event.preventDefault();
-    const texto = inputTexto.value;
-    buscarHerramienta(texto);
-  });
-
-  // Agregar evento de búsqueda por fecha
-  inputFecha.addEventListener('change', () => {
-    const fecha = inputFecha.value;
-    buscarPorFecha(fecha);
-  });
-
-  // Agregar evento para filtrar por categoría
-  selectCategoria.addEventListener('change', (event) => {
-    const categoria = event.target.value;
-    buscarPorCategoria(categoria);
+    var id = document.getElementById("editId").value;
+    var herramienta = historialAlmacen.find(item => item.id == id);
+    herramienta.nombre = document.getElementById("editNombre").value;
+    herramienta.marca = document.getElementById("editMarca").value;
+    herramienta.tamaño = document.getElementById("editTamano").value;
+    herramienta.ordenCompra = document.getElementById("editOrdenCompra").value;
+    herramienta.noSerie = document.getElementById("editSerie").value;
+    herramienta.estado = document.getElementById("editEstado").value;
+    herramienta.color = document.getElementById("editColor").value;
+    herramienta.tipo = document.getElementById("editTipo").value;
+    herramienta.fechaRegistro = document.getElementById("editFecha").value;
+    herramienta.descripcion = document.getElementById("editDescripcion").value;
+    generarTablaHistorial(historialAlmacen);
+    popup.style.display = "none";
+    cerrarPopup();
   });
 });
 
+function editarHerramienta(id) {
+  var herramienta = historialAlmacen.find(item => item.id == id);
+  document.getElementById("editId").value = herramienta.id;
+  document.getElementById("editNombre").value = herramienta.nombre;
+  document.getElementById("editMarca").value = herramienta.marca;
+  document.getElementById("editTamano").value = herramienta.tamaño;
+  document.getElementById("editOrdenCompra").value = herramienta.ordenCompra;
+  document.getElementById("editSerie").value = herramienta.noSerie;
+  document.getElementById("editEstado").value = herramienta.estado;
+  document.getElementById("editColor").value = herramienta.color;
+  document.getElementById("editTipo").value = herramienta.tipo;
+  document.getElementById("editFecha").value = herramienta.fechaRegistro;
+  document.getElementById("editDescripcion").value = herramienta.descripcion;
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+  abrirPopup();
+}
+
+function generarTablaHistorial(data) {
+  const tabla = document.getElementById('tabla-historial').getElementsByTagName('tbody')[0];
+  tabla.innerHTML = "";
+  data.forEach(item => {
+    const newRow = tabla.insertRow();
+    newRow.innerHTML = `
+      <td>${item.id}</td>
+      <td>${item.nombre}</td>
+      <td>${item.marca}</td>
+      <td>${item.ordenCompra}</td>
+      <td>${item.tamaño}</td>
+      <td>${item.noSerie}</td>
+      <td>${item.estado}</td>
+      <td>${item.color}</td>
+      <td>${item.tipo}</td>
+      <td>${item.fechaRegistro}</td>
+      <td>${item.descripcion}</td>
+      <td><button class="accion-button"onclick="editarHerramienta(${item.id})">Editar</button>
+      <button class="accion-button"onclick="DescargarQR(${item.id})">DescargarQR</button>
+      </td>
+      
+    `;
+  });
+}
+
+function buscarHerramienta(texto) {
+  const resultados = historialAlmacen.filter(item => 
+    item.id == texto || 
+    item.nombre.toLowerCase().includes(texto.toLowerCase()) ||
+    item.marca.toLowerCase().includes(texto.toLowerCase())||
+    item.ordenCompra.toLowerCase().includes(texto.toLowerCase()) ||
+    item.tamaño.toLowerCase().includes(texto.toLowerCase()) ||
+    item.estado.toLowerCase().includes(texto.toLowerCase()) ||
+    item.descripcion.toLowerCase().includes(texto.toLowerCase()) ||
+    item.color.toLowerCase().includes(texto.toLowerCase()) ||
+    item.noSerie.toLowerCase().includes(texto.toLowerCase())
+  );
+  generarTablaHistorial(resultados);
+}
 
 
+function buscarPorCategoria(categoria) {
+  const resultados = historialAlmacen.filter(item => 
+    categoria == "todos" || item.tipo == categoria
+  );
+  generarTablaHistorial(resultados);
+}
+
+function buscarPorFecha(fecha) {
+  const resultados = historialAlmacen.filter(item => 
+    item.fechaRegistro == fecha
+  );
+  generarTablaHistorial(resultados);
+}
+
+function abrirPopup() {
+  document.getElementById('overlay').style.display = 'block';
+  document.getElementById('popup').style.display = 'block';
+}
+
+function cerrarPopup() {
+  document.getElementById('overlay').style.display = 'none';
+  document.getElementById('popup').style.display = 'none';
+}
