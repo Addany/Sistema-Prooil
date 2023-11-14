2242482959453a5023e95ac8d5fc0e8fff3511d8
Addany <appdata.123@gmail.com>
2023-11-14 13:56:07 -0600

    correccion de estilos

--- js/registroepp.js
+++ js/registroepp.js
@@ -1,33 +1,22 @@
 $(document).ready(function() {
-    // Inicializar Select2 en el select de nombre y permitir nuevas entradas
-    $('#nombre').select2({
+    // Inicializar Select2
+    $('#nombre, #clase').select2({
         tags: true,
-        placeholder: 'Selecciona un EPP',
+        placeholder: function(){
+            $(this).data('placeholder');
+        },
         allowClear: true
-    })
-    .on('select2:open', function() {
-        // Cuando se abre, establece el placeholder del campo de búsqueda
-        $('input.select2-search__field').attr('placeholder', 'Agrega un nuevo EPP');
+    }).on('select2:open', function() {
+        // Ajustar el ancho del select a su contenedor
+        var container = $(this).parent().get(0);
+        var containerWidth = $(container).outerWidth();
+        $('.select2-container').css('width', containerWidth);
+        
+        // Ajustes adicionales para cuando se abre el select
+        $('input.select2-search__field').attr('placeholder', 'Agrega un nuevo elemento');
         $('body').addClass('select-open');
-    })
-    .on('select2:close', function() {
-        // Cuando se cierra, elimina la clase 'select-open'
-        $('body').removeClass('select-open');
-    });
-
-    // Inicializar Select2 en el select de clase con las mismas funcionalidades
-    $('#clase').select2({
-        tags: true, // Permite la creación de nuevas opciones al escribir
-        placeholder: 'Selecciona una clase', // Placeholder para el select
-        allowClear: true // Permite borrar la selección
-    })
-    .on('select2:open', function() {
-        // Cuando se abre, establece el placeholder del campo de búsqueda
-        $('input.select2-search__field').attr('placeholder', 'Agrega una nueva clase');
-        $('body').addClass('select-open');
-    })
-    .on('select2:close', function() {
-        // Cuando se cierra, elimina la clase 'select-open'
+    }).on('select2:close', function() {
+        // Ajustes cuando se cierra el select
         $('body').removeClass('select-open');
     });
 });
